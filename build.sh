eb965ec81b6d589cc1586255af39130394d09ebb
Ricardo Joaquinito <rjoaquinito@gmail.com>
2019-11-08 22:58:18 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
+# If non-existant, create folder 'bin'
 if [ ! -d "bin" ]; then
     mkdir bin;
 fi
 
+# If non-existant, create folder 'build'
+if [ ! -d "build" ]; then
+    mkdir build;
+fi
+
 cd build
+
+# If Google Test has not been cloned yet, clone from Github
+if [ ! -d "googletest" ]; then
+   git clone https://github.com/google/googletest.git;
+fi
+
+# Run CMake
 cmake ..
+# Run makefiles
 make
